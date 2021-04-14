05523a15aceb89a7db7d13bff075b6fc904241d7
kwamitchamounelson <kwamitchamounelson@gmail.com>
2021-04-14 14:59:43 +0100

    init trigger function

--- functions/modules/groupSaving/groupSavingMain.js
+++ functions/modules/groupSaving/groupSavingMain.js
@@ -1,35 +1,46 @@
 const functions = require("firebase-functions")
 const utils = require("../../utils/utils")
-const constants = require("../../utils/Constants")
-const notification = require("../../FCM/main")
+require("../../utils/Constants")
+const notificationModule = require("../../FCM/main")
+const dataTopicEntity = require('../../entities/DataTopic');
+const dataTokenEntity = require('../../entities/DataToken');
+
+const GROUP_ENTITY_NAME = "GroupSaving"
 
 exports.listenGroup = functions.firestore
     .document("GROUP_SAVING/{groupId}")
     .onWrite((change, contex) => {
 
-        if (change.before.existes()) {
+        if (change.before.exists && change.after.exists) {
             // update event
-            return
-        } else if (!change.after.exists()) {
+            console.log("********* UPDATE *****************")
+            let docRef = change.after.ref
+            let path = docRef.path
+            let data = new dataTopicEntity.DataTopic(path, "UPDATE", GROUP_ENTITY_NAME)
+            let topic = docRef.id
+
+            console.log("Document Path-> " + path)
+            console.log("Topic-> " + topic)
+            console.log("Data-> " + { ...data })
+            //notificationModule.sendMessageToTopicForSync(topic, data)
+
+        } else if (!change.after.exists) {
             //delete event
-            return null;
+            console.log("********* DELETE *****************")
+            let docRef = change.before.ref
+            let path = docRef.path
+            let data = new dataTopicEntity.DataTopic(path, "DELETE", GROUP_ENTITY_NAME)
+            let topic = docRef.id
+
+            console.log("Document Path-> " + path)
+            console.log("Topic-> " + topic)
+            console.log("Data-> " + { ...data })
+            //notificationModule.sendMessageToTopicForSync(topic, data)
         } else {
             //add event
+            console.log("********* CREATE *****************")
         }
 
     });
-
-
-function processCreate(change) {
-
-    let documentPath = change.ref.path()
-    console.log("Document Path" + documentPath);
-
-    let data = new dataTopicEntity.DataTopic(documentPath, constants.CREATE, "GroupSaving")
-    let topic = "KOLA"
-
-    notificationModule.sendMessageToTopicForSync(topic, data)
-
-}
 
 
--- functions/test.js
+++ functions/test.js
@@ -10,8 +10,6 @@ exports.sendKolaNotification = functions.https.onRequest((req, res) => {
 });
 
 exports.sendNotificationToTopic = functions.https.onRequest((req, res) => {
-    let imageUrl = "https://firebasestorage.googleapis.com/v0/b/kola-wallet-dev.appspot.com/o/FCMImages%2Fbitmoji-20190131055305.png?alt=media&token=cfe2f5b6-7ef9-4926-825d-ed600aea07eb"
-    //let notification = new notifEntity.Notification("Bonjour c'est KOLA", "Message de notification topic avec data", imageUrl)
     let data = new dataTopicEntity.DataTopic("KUSER/yeyeyeye/GROUPESAVING", "DELETE", "Group")
     let topic = "KOLA"
 
