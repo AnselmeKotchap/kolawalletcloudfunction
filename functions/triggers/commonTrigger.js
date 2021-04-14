34ab77d1a7cb21b171713ea2468ae5b5fc6a0143
tsafix <tsafackdcedrick@gmail.com>
2021-04-14 17:46:49 +0100

    update some files

--- functions/triggers/commonTrigger.js
+++ functions/triggers/commonTrigger.js
@@ -8,7 +8,7 @@ const dataTokenEntity = require('../entities/DataToken');
 exports.onWriteTrigger = function (documentRefPath, className) {
     return functions.firestore
         .document(documentRefPath)
-        .onWrite((change, contex) => {
+        .onWrite((change, context) => {
             console.log("############## documentRefPath-> " + documentRefPath + " ##############")
             console.log("############## className-> " + className + " ##############")
             if (change.before.exists && change.after.exists) {
@@ -57,4 +57,63 @@ exports.onWriteTrigger = function (documentRefPath, className) {
             }
 
         });
+}
+
+
+exports.onWriteMemberTrigger = function (documentRefPath, className) {
+    return functions.firestore.document(documentRefPath)
+    .onWrite((change, context) => {
+        console.log("############## documentRefPath-> " + documentRefPath + " ##############")
+        console.log("############## className-> " + className + " ##############")
+        if (change.before.exists && change.after.exists) {
+            // update event
+            console.log("********* UPDATE *****************")
+            let docRef = change.after.ref
+            let path = docRef.path
+            let data = new dataTopicEntity.DataTopic(path, "UPDATE", className)
+            let topic = docRef.id
+
+            console.log("Document Path-> " + path)
+            console.log("Topic-> " + topic)
+            console.log("Data path -> " + data.path)
+            console.log("Data method -> " + data.method)
+            console.log("Data entity -> " + data.entity)
+            notificationModule.sendMessageToTopicForSync(topic, data)
+
+        } else if (!change.after.exists) {
+            //delete event
+            console.log("********* DELETE *****************")
+            let docRef = change.before.ref
+            let path = docRef.path
+            let data = new dataTopicEntity.DataTopic(path, "DELETE", className)
+            let topic = docRef.id
+
+            console.log("Document Path-> " + path)
+            console.log("Topic-> " + topic)
+            console.log("Data path -> " + data.path)
+            console.log("Data method -> " + data.method)
+            console.log("Data entity -> " + data.entity)
+            notificationModule.sendMessageToTopicForSync(topic, data)
+        } else {
+            //add event
+            console.log("********* CREATE *****************")
+            let docRef = change.after.ref
+            let path = docRef.path
+            let data = new dataTopicEntity.DataTopic(path, "CREATE", className)
+            let memberUserId = context.params.userIdOfMember
+            let memberMaintPhoneNmber =context.params.mainPhone
+            let token = memberUserId
+
+            console.log("Document Path-> " + path)
+            console.log("Token-> " + token)
+            console.log("Data path -> " + data.path)
+            console.log("Data method -> " + data.method)
+            console.log("Data entity -> " + data.entity)
+            console.log("New member authPhoneNumber ->"+memberUserId)
+            console.log("New member user Id ->"+memberMaintPhoneNmber)
+
+            notificationModule.sendMessageToTokenForSync(token, data)
+        }
+
+    });
 }
